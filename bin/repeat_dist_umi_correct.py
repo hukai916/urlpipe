9ffbd65b47ca399d8d13ade7a0676682dd05e898
Kai Hu <>
2022-09-19 22:00:26 -0400

    minor

--- bin/repeat_dist_umi_correct.py
+++ bin/repeat_dist_umi_correct.py
@@ -47,9 +47,7 @@ with open(outfile_mode, "w") as f:
 
 # output to plot:
 outplot_mean = os.path.join(outdir, "plot_mean_" + sample_name + "_cutoff_" + str(cutoff) + ".png")
-os.makedirs(outplot_mean, exist_ok=True)
 plot_repeat_dist(outfile_mean, outplot_mean, sample_name, N)
 
 outplot_mode = os.path.join(outdir, "plot_mode_" + sample_name + "_cutoff_" + str(cutoff) + ".png")
-os.makedirs(outplot_mode, exist_ok=True)
 plot_repeat_dist(outfile_mode, outplot_mode, sample_name, N)
