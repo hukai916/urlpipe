05dd5022f00c6ddff9bb1531277a301a000e7c08
Kai Hu <>
2022-09-19 21:46:37 -0400

    minor

--- bin/repeat_dist_umi_correct.py
+++ bin/repeat_dist_umi_correct.py
@@ -14,6 +14,7 @@ tsv         = sys.argv[1]
 sample_name = sys.argv[2]
 outdir      = sys.argv[3]
 cutoff      = sys.argv[4]
+cutoff      = int(cutoff)
 
 repeat_length_mean = {}
 repeat_length_mode = {}
@@ -21,17 +22,18 @@ repeat_length_mode = {}
 with open(tsv, "r") as f:
     for line in f:
         umi, count, mean, mode = line.split()
-        if count >= int(cutoff):
-            if not int(mean) in repeat_length_mean:
-                repeat_length_mean[int(mean)] = 1
+        count, mean, mode = int(count), int(mean), int(mode)
+        if count >= cutoff:
+            if not mean in repeat_length_mean:
+                repeat_length_mean[mean] = 1
             else:
-                repeat_length_mean[int(mean)] += 1
+                repeat_length_mean[mean] += 1
 
-            if count >= int(cutoff):
-                if not int(mode) in repeat_length_mode:
-                    repeat_length_mode[int(mode)] = 1
+            if count >= cutoff:
+                if not mode in repeat_length_mode:
+                    repeat_length_mode[mode] = 1
                 else:
-                    repeat_length_mode[int(mode)] += 1
+                    repeat_length_mode[mode] += 1
 # output to stat:
 with open(os.path.join(outdir, "stat_mean_" + smaple_name + "_cutoff_" + str(cutoff) + ".tsv"), "w") as f:
     for k in sorted(repeat_length_mean.keys()):
