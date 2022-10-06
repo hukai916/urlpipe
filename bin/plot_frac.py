9aa6b51597e29e883abddc6b8dabf3dc764f2946
Kai Hu <>
2022-10-05 22:43:31 -0400

    minor

--- bin/plot_frac.py
+++ bin/plot_frac.py
@@ -13,6 +13,22 @@ csv = sys.argv[1]
 outfile = sys.argv[2]
 
 data=pd.read_csv(csv, sep=',')
+
+fig, ax = plt.subplots()
+labels = list(data.iloc[:,0])
+below_mean = list(data.iloc[:,2])
+above_mean = list(data.iloc[:,6])
+width = 0.35
+
+ax.bar(labels, below_mean, width, label='below_frac')
+ax.bar(labels, above_mean, width, bottom=below_mean,
+       label='above_frac')
+ax.set_ylabel('Fraction')
+ax.set_title('Below and Above Cutoff Reads Fraction by Sample')
+ax.legend()
+plt.xticks(rotation = 85)
+plt.savefig(output_file, dpi = 300, bbox_inches = "tight")
+plt.close()
 # x = data.iloc[:,0].to_frame()
 # weight = np.log2(data[["count"]])
 #
