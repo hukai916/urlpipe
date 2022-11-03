014dd1b6e06a54ba36278bd4ccc67c0a87a376bf
Kai Hu <>
2022-11-03 14:24:13 -0400

    minor

--- bin/plot_frac_umi_cutoff.py
+++ bin/plot_frac_umi_cutoff.py
@@ -9,8 +9,8 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import glob
 from pathlib import Path
+import glob
 
 sample_name = sys.argv[1]
 csv_no_correction = sys.argv[2]
@@ -18,21 +18,27 @@ umi_cutoff  = sys.argv[3]
 outfile     = sys.argv[4]
 
 umis = [x.strip() for x in umi_cutoff.split(",")]
-res = [sum(pd.read_csv(csv_no_correction, header = None).iloc[:, 1])]
-for umi in umis:
-    file = "stat_mode_" + sample_name + "_cutoff_" + umi + ".csv"
-    df = pd.read_csv(file, header = None)
-    res.append(sum(df.iloc[:, 1]))
-res[0] = int(res[0]/10)
+res  = pd.read_csv(raw_csv, header = None).iloc[:. [2,4]] # above/below percentage columns
+for x in umis:
+    filename = sample_name + "_frac_*_cutoff_" + x + ".csv"
+    file = glob.glob(filename)[0]
+    res = pd.concat([res, pd.read_csv(file, header = None).iloc[:, [2,6]]], ignore_index = True)
 
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(1,2)
 width = 0.35
 label = ["Cutoff_" + x for x in umis]
-label = ["No_correction\n (count/10)"] + label
+label = ["No_correction"] + label
 
-ax.bar(label, res, width)
-ax.set_ylabel("Counts")
-ax.set_title("UMI group count vs UMI cutoff")
-plt.xticks(rotation = 10)
+ax1.bar(label, res.iloc[:, 0], width)
+ax1.set_ylabel("Percentage")
+ax1.set_title("Above cutoff fraction")
+ax1.set_xticklabels(label, rotation = 25, fontsize = 8)
 
-fig.savefig(outfile)
+ax2.bar(label, res.iloc[:, 1], width)
+ax2.set_ylabel("Percentage")
+ax2.set_title("Below fraction cutoff")
+ax2.set_xticklabels(label, rotation = 25, fontsize = 8)
+
+fig.tight_layout()
+
+fig.savefig(outfile, dpi = 600)
