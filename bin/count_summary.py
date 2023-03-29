7e514ed469350404a329f2cd28f89c2ecca557ec
hukai916 <31452631+hukai916@users.noreply.github.com>
2023-03-29 12:50:36 -0400

    minor

--- bin/count_summary.py
+++ bin/count_summary.py
@@ -21,4 +21,16 @@ df = df.loc[:,~df.columns.duplicated()]
 df = df.rename(columns={"sample_namesample_name": "sample_name", "read_count": "indel_count"})
 
 df_res = df[["sample_name", "indel_count", "below_count", "between_count", "above_count"]]
+df_res = df_res.rename(columns={'below_count': 'below',
+                                'between_count': 'allele_1',
+                                'above_count': 'above',
+                                'indel_count': 'indel'
+                                })
+
+df_res['total'] = df_res.sum(axis=1)
+for col in df_res.columns[:-1]:
+    df_res[col + '_frac'] = df_res[col] / df_res['total']
+
+cols_in_order = ["indel", "indel_frac", "below", "below_frac", "allele_1", "allele_1_frac", "above", "above_frac", "total"]
+df_res = df_res[cols_in_order]
 df_res.to_csv(outfile, index = False)
