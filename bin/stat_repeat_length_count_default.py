bac3138bec6a4e754ecd28ebb15a317558b11969
hukai916 <31452631+hukai916@users.noreply.github.com>
2023-04-05 15:00:25 -0400

    minor

--- bin/stat_repeat_length_count_default.py
+++ bin/stat_repeat_length_count_default.py
@@ -9,12 +9,13 @@ outfile_csv = sys.argv[-1]
 
 sample_names = [get_sample_name(x, "repeat_length_count_", ".csv") for x in csv]
 
-df_res = pd.DataFrame("repeat_length": [])
+df_res = pd.DataFrame(columns = ["repeat_length"])
 
 for i in range(len(csv)):
     df = pd.read_csv(csv[i])
-    df = df.rename(columns={'count': sample_name[i]})
+    df = df.rename(columns={'count': sample_names[i]})
     df_res = pd.merge(df_res, df, on='repeat_length', how='outer')
     df_res.fillna(0, inplace = True)
 
+df_res = df_res.sort_values('repeat_length')
 df_res.to_csv(outfile_csv, index = False)
