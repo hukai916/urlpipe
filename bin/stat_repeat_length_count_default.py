bfd88a4d8ed632ffa506795d0253f1149a5eafab
hukai916 <31452631+hukai916@users.noreply.github.com>
2023-04-06 21:05:50 -0400

    minor

--- bin/stat_repeat_length_count_default.py
+++ bin/stat_repeat_length_count_default.py
@@ -7,7 +7,7 @@ import plotly.graph_objs as go
 import plotly.offline as pyo
 
 
-csv = sys.argv[1:-2]
+csv = sys.argv[1:-5]
 outfile_csv = sys.argv[-4]
 outfile_html = sys.argv[-3]
 prefix = sys.argv[-2]
