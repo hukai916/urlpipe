63845820ebf973970c358e99a8fbaa713918afc3
hukai916 <31452631+hukai916@users.noreply.github.com>
2023-04-06 23:37:16 -0400

    minor

--- bin/repeat_length_fraction.py
+++ bin/repeat_length_fraction.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 """
-Calculate the fraction of reads given four cutoffs.
-
-sample_name, below_count, below_frac, below_mean, below_sd, between_count, between_frac, between_sd, above_count, above_frac, above_mean, above_sd, between_count_2, between_frac_2, between_sd_2, above_count_2, above_frac_2, above_mean_2, above_sd_2
-
-(below_count) cutoff_1 (bewteen_count) cutoff_2 (above_count | below_2_count) cutoff_3 (between_count_2) cutoff_4 (above_count_2)
+Dev notes:
+1.  pd.cut follows: (] range by default
 
 """
 
@@ -14,7 +11,7 @@ import os
 import numpy as np
 import pandas as pd
 
-allele_number  = sys.argv[1]
+allele_number  = int(sys.argv[1])
 sample_name    = sys.argv[2]
 repeat_length_count_table = sys.argv[3]
 outfile        = sys.argv[4]
@@ -23,36 +20,35 @@ end_allele_1   = sys.argv[6]
 start_allele_2 = sys.argv[7]
 end_allele_2   = sys.argv[8]
 
-df = pd.read_csv(repeat_length_count_table)
-print(df)
+# 1. read in sample column into df and add bin:
+df = pd.read_csv(repeat_length_count_table)[["repeat_length", sample_name]]
+if allele_number == 1:
+    bins = [min(df["repeat_length"]) - 1, int(start_allele_1), int(end_allele_1), max(df["repeat_length"]) + 1]
+elif allele_number == 2:
+    bins = [min(df["repeat_length"]) - 1, int(start_allele_1), int(end_allele_1), int(start_allele_2), int(end_allele_2), max(df["repeat_length"]) + 1]
+df["bin"] = pd.cut(df["repeat_length"], bins = bins)
 
+# 2. calculate count, fraction, mean, std:
+_total_count = df[sample_name].sum()
+df["weighted_length"] = df["repeat_length"] * df[sample_name]
 
-# if os.path.getsize(csv) > 0:
-#     # csv = "30"
-#     df = pd.read_csv(csv, header = None, names = ["length", "frequency"])
-#     # skip "plus" and "problem" reads because they make it tricky to calculate the mean length
-#     df["length"] = pd.to_numeric(df["length"], errors = 'coerce')
-#     df.dropna(subset=["length"], inplace = True)
-#     bins = [0, cutoff_below, cutoff_above, cutoff_below_2, cutoff_above_2, np.inf]
-#     df["bin"] = pd.cut(df["length"], bins=bins)
-#     res = str(sample_name) + ","
-#     # calculate count, fraction, mean, std:
-#     _total_count = df["frequency"].sum()
-#     df["weighted_length"] = df["length"] * df["frequency"]
-#
-#     _count = df.groupby("bin")["frequency"].sum()
-#     _fraction = _count / _total_count
-#     _mean = df.groupby("bin")["weighted_length"].sum() / df.groupby("bin")["frequency"].sum()
-#     _std = df.groupby("bin")["length"].agg(lambda x: np.sqrt(np.average((x - np.average(x, weights = df.loc[x.index, "frequency"])) ** 2, weights = df.loc[x.index, "frequency"])))
-#
-#     for i in range(len(_count)):
-#         res = res + ",".join([str(_count.iloc[i]), str(_fraction.iloc[i]), str(_mean.iloc[i]), str(_std.iloc[i])])
-#         if not i == len(_count) - 1:
-#             res = res + ","
-# else:
-#     res = ",".join(["nan"] * 20)
-#
-# outfile = os.path.join(outdir, sample_name + "_frac_" + str(cutoff_below) + "_" + str(cutoff_above) + "_cutoff_" + str(umi_cutoff) + ".csv")
-# with open(outfile, "w") as f:
-#     f.write(res + "\n")
-#     # print(res)
+_count = df.groupby("bin")[sample_name].sum()
+_fraction = _count / _total_count
+_mean = df.groupby("bin")["weighted_length"].sum() / df.groupby("bin")[sample_name].sum()
+_std = df.groupby("bin")["repeat_length"].agg(lambda x: np.sqrt(np.average((x - np.average(x, weights = df.loc[x.index, sample_name])) ** 2, weights = df.loc[x.index, sample_name])))
+
+# 3. output to csv:
+res = ""
+if allele_number == 1:
+    header = "below,allele_1,above_allele_1"
+elif allele_number == 2:
+    header = "below,allele_1,between,allele_2,above"
+for i in range(len(_count)):
+    # res = res + ",".join([str(_count.iloc[i]), str(_fraction.iloc[i]), str(_mean.iloc[i]), str(_std.iloc[i])])
+    res = res + str(_count.iloc[i])
+    if not i == len(_count) - 1:
+        res = res + ","
+
+with open(outfile, "w") as f:
+    f.write(header + "\n")
+    f.write(res)
