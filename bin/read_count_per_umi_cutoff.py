6d6625021c6be55db878747104d90d3c1f7b43ab
hukai916 <31452631+hukai916@users.noreply.github.com>
2023-04-07 22:27:33 -0400

    minor

--- bin/read_count_per_umi_cutoff.py
+++ bin/read_count_per_umi_cutoff.py
@@ -13,7 +13,7 @@ outfile    = sys.argv[3]
 
 df = pd.read_csv(umi_reads, header = None, names = ["umi", "read_count"])
 df_res = df[df["read_count"] >= umi_cutoff]
-res = df_res["read_count"].sum()
+res = len(df_res["read_count"])
 
 with open(outfile, "w") as f:
     f.write(str(res))
