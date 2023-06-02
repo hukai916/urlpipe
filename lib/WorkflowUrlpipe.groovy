eca966f7c1f3d778d99c894abe7c0ab69d3ac860
hukai916 <31452631+hukai916@users.noreply.github.com>
2023-06-02 11:43:02 -0400

    minor

--- lib/WorkflowUrlpipe.groovy
+++ lib/WorkflowUrlpipe.groovy
@@ -8,12 +8,10 @@ class WorkflowUrlpipe {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-
-
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
-        }
+        // if (!params.fasta) {
+        //     log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
+        //     System.exit(1)
+        // }
     }
 
     //
