9490ace7622f0a6010425ffb834f47e0959c11a1
hukai916 <31452631+hukai916@users.noreply.github.com>
2023-06-20 21:39:10 -0400

    minor

--- bin/stat_repeat_length_count_default.py
+++ bin/stat_repeat_length_count_default.py
@@ -21,6 +21,13 @@ xlim   = sys.argv[-1]
 # prefix = "repeat_length_count_"
 # suffix = ".csv"
 
+prefix = "repeat_length_count_default_"
+suffix = "_umi_100.csv"
+outfile_csv = "test.csv"
+xlim = 500
+outfile_html = "test.html"
+csv = ['repeat_length_count_default_hs_BC01_umi_100.csv', 'repeat_length_count_default_hs_BC13_umi_100.csv', 'repeat_length_count_default_hs_BC02_umi_100.csv', 'repeat_length_count_default_hs_BC14_umi_100.csv', 'repeat_length_count_default_hs_BC03_umi_100.csv', 'repeat_length_count_default_hs_BC15_umi_100.csv', 'repeat_length_count_default_hs_BC04_umi_100.csv', 'repeat_length_count_default_hs_BC16_umi_100.csv', 'repeat_length_count_default_hs_BC05_umi_100.csv', 'repeat_length_count_default_hs_BC17_umi_100.csv', 'repeat_length_count_default_hs_BC06_umi_100.csv', 'repeat_length_count_default_hs_BC18_umi_100.csv', 'repeat_length_count_default_hs_BC07_umi_100.csv', 'repeat_length_count_default_hs_BC19_umi_100.csv', 'repeat_length_count_default_hs_BC08_umi_100.csv', 'repeat_length_count_default_hs_BC20_umi_100.csv', 'repeat_length_count_default_hs_BC09_umi_100.csv', 'repeat_length_count_default_hs_BC21_umi_100.csv', 'repeat_length_count_default_hs_BC10_umi_100.csv', 'repeat_length_count_default_hs_BC22_umi_100.csv', 'repeat_length_count_default_hs_BC11_umi_100.csv', 'repeat_length_count_default_hs_BC23_umi_100.csv', 'repeat_length_count_default_hs_BC12_umi_100.csv']
+
 sample_names = [get_sample_name(x, prefix, suffix) for x in csv]
 
 df_res = pd.DataFrame(columns = ["repeat_length"])
@@ -32,6 +39,9 @@ for i in range(len(csv)):
     df_res.fillna(0, inplace = True)
 
 df_res = df_res.sort_values('repeat_length')
+
+# move 'repeat_length' as col0:
+df_res.insert(0, 'repeat_length', df_res.pop('repeat_length'))
 
 # 1. output repeat_length_count_default_umi_0.csv
 df_res.to_csv(outfile_csv, index = False)
@@ -49,8 +59,11 @@ bars = [go.Bar(x = df_res["repeat_length"],
 fig = go.Figure(data = bars)
 
 # x_range = [min(df["repeat_length"]) - 1, min(int(xlim), max(df["repeat_length"]) + 100 )]
-x_range = [0, min(int(xlim), max(df["repeat_length"]) + 100 )]
-
+if df_res.empty:
+    x_range = [0, min(int(xlim), 100 )]
+else:
+    x_range = [0, min(int(xlim), max(df["repeat_length"]) + 100 )]
+    
 nbinsx  = x_range[1] - x_range[0] + 1
 x_ticks  = int(nbinsx / 5)
 
